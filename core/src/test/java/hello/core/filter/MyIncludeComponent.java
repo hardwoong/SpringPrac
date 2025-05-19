372d6c20979cc164b669e9cdaafc81ac7c5ff10d
hardwoong <dew04300@naver.com>
2025-05-19 21:30:29 +0900

    feat: section7

---
+++ core/src/test/java/hello/core/filter/MyIncludeComponent.java
@@ -0,0 +1,12 @@
+package hello.core.filter;
+
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Component
+public @interface MyIncludeComponent {
+}
