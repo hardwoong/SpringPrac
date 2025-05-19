d497f2569423cef05d2bf1b2238d38698608a9e5
hardwoong <dew04300@naver.com>
2025-05-19 13:13:22 +0900

    section5

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -14,19 +14,29 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
+  // @Bean memberService -> new MemoryMemberRepository
+  // @Bean orderService -> new MemoryMemberRepository
 
+  // call AppConfig.memberService
+  // call AppConfig.memberRepository
+  // call AppConfig.memberRepository
+  // call AppConfig.orderService
+  // call AppConfig.memberRepository
   @Bean
   public MemberService memberService() {
+    System.out.println("call AppConfig.memberService");
     return new MemberServiceImpl(memberRepository()); // 생성자 주입
   }
 
   @Bean
-  public static MemberRepository memberRepository() {
+  public MemberRepository memberRepository() {
+    System.out.println("call AppConfig.memberRepository");
     return new MemoryMemberRepository();
   }
 
   @Bean
   public OrderService orderService() {
+    System.out.println("call AppConfig.orderService");
     return new OrderServiceImpl(memberRepository(), discountPolicy());
   }
 
